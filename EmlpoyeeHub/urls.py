e5b471cb2084be09d2c7c43c5979912555b0b74a
Kateřina Klimková <katerina.klimkova@email.cz>
2024-09-18 21:36:17 +0200

    The Users module is registered in the urls.py file

--- EmlpoyeeHub/urls.py
+++ EmlpoyeeHub/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from viewer.models import Users
+
+admin.site.register(Users)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
