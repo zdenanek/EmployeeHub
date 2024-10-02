7d2626e236c16597794e69fef3f2381dd28df4a7
zdenanek <zdenek.kubik@protonmail.com>
2024-10-02 20:05:58 +0200

    feat: CRUD subcontract...

--- viewer/forms.py
+++ viewer/forms.py
@@ -5,34 +5,34 @@ from viewer.models import Contract, Customer, SubContract
 
 
 class SignUpForm(UserCreationForm):
-  class Meta(UserCreationForm.Meta):
-    fields = ['username', 'first_name', 'last_name', 'email']
+    class Meta(UserCreationForm.Meta):
+        fields = ['username', 'first_name', 'last_name', 'email']
 
-  def save(self, commit=True):
-    self.instance.is_active = False
-    return super().save(commit)
+    def save(self, commit=True):
+        self.instance.is_active = False
+        return super().save(commit)
 
 
 class ContractForm(ModelForm):
-  class Meta:
-    model = Contract
-    fields = "__all__"
+    class Meta:
+        model = Contract
+        fields = "__all__"
 
 
 class CustomerForm(ModelForm):
-  class Meta:
-    model = Customer
-    fields = "__all__"
+    class Meta:
+        model = Customer
+        fields = "__all__"
 
 
 class SubContractForm(ModelForm):
-  class Meta:
-    model = SubContract
-    fields = ['subcontract_name']
-    # fields = '__all__'
+    class Meta:
+        model = SubContract
+        fields = ['subcontract_name']
+        # fields = '__all__'
 
 
 class SubContractFormUpdate(ModelForm):
-  class Meta:
-    model = SubContract
-    fields = '__all__'
+    class Meta:
+        model = SubContract
+        fields = '__all__'
