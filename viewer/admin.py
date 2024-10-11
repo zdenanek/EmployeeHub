ba6288577b2ee3ba66b92d0e710ec5aa1da8e4b3
Belys <michael.belka.mb@gmail.com>
2024-10-11 21:51:45 +0200

    feat: finish employee_profile.html, change buttons to custom

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
-from .models import Position, UserProfile
+from .models import Position, UserProfile, EmployeeInformation, BankAccount, EmergencyContact, Contract, Customer, Groups, SubContract, Comment, Event
+
+class EmergencyContactInline(admin.TabularInline):
+    model = EmergencyContact
+    extra = 1  # Number of empty forms to display
+
+
+class BankAccountInline(admin.StackedInline):
+    model = BankAccount
+    can_delete = False
+    verbose_name_plural = 'Bank Account'
+
+
+class EmployeeInformationInline(admin.StackedInline):
+    model = EmployeeInformation
+    can_delete = False
+    verbose_name_plural = 'Employee Information'
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'position', 'phone_number']
+    inlines = [EmployeeInformationInline, BankAccountInline, EmergencyContactInline]
+
 
 admin.site.register(Position)
-admin.site.register(UserProfile)
+admin.site.register(Customer)
+admin.site.register(Contract)
+admin.site.register(Groups)
+admin.site.register(SubContract)
+admin.site.register(Comment)
+admin.site.register(Event)
 
--- viewer/forms.py
+++ viewer/forms.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import CharField
-from django.forms import ModelForm, Form, CharField
+from django.forms import ModelForm, Form, CharField, inlineformset_factory, BaseInlineFormSet
 from django import forms
-
+from django.core.validators import RegexValidator
+from .models import UserProfile, BankAccount, EmployeeInformation, EmergencyContact
 
 from viewer.models import Contract, Customer, SubContract, Comment
 
@@ -49,3 +50,73 @@ class CommentForm(ModelForm):
 
 class SearchForm(forms.Form):
     query = forms.CharField(label="Search", max_length=256)
+
+
+class BankAccountForm(forms.ModelForm):
+    account_number = forms.CharField(
+        max_length=20,
+        validators=[RegexValidator(r'^\d+$', 'Pole musí obsahovat pouze číslice')]
+    )
+    account_prefix = forms.CharField(
+        max_length=6,
+        validators=[RegexValidator(r'^\d+$', 'Pole musí obsahovat pouze číslice')]
+    )
+    bank_code = forms.CharField(
+        max_length = 4,
+        validators = [RegexValidator(r'^\d+$', 'Pole musí obsahovat pouze číslice')]
+
+)
+
+    class Meta:
+        model = BankAccount
+        exclude = ('user_profile',)
+
+
+class EmergencyContactForm(forms.ModelForm):
+    class Meta:
+        model = EmergencyContact
+        exclude = ('user_profile',)
+
+class BaseEmergencyContactFormSet(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        total_forms = 0
+        for form in self.forms:
+            if form.cleaned_data and not form.cleaned_data.get('DELETE', False):
+                # Check if any field is filled
+                filled = any(value for key, value in form.cleaned_data.items() if key != 'DELETE' and value)
+                if filled:
+                    total_forms += 1
+        if total_forms > 2:
+            raise forms.ValidationError("You can only have up to two emergency contacts.")
+
+EmergencyContactFormSet = inlineformset_factory(
+    UserProfile,
+    EmergencyContact,
+    form=EmergencyContactForm,
+    formset=BaseEmergencyContactFormSet,
+    extra=2,
+    max_num=2,
+    can_delete=True,
+)
+
+
+class EmployeeInformationForm(forms.ModelForm):
+    permament_postal_code = forms.CharField(
+        max_length=5,
+        validators=[RegexValidator(r'^\d+$', 'Pole musí obsahovat pouze číslice')]
+    )
+    phone_number = forms.CharField(
+        max_length=15,
+        validators=[RegexValidator(r'^\d+$', 'Pole musí obsahovat pouze číslice')]
+    )
+    class Meta:
+        model = EmployeeInformation
+        # exclude = ('user_profile',)
+        fields = [
+            'permament_address',
+            'permament_descriptive_number',
+            'permament_postal_code',
+            'city',
+            'phone_number',
+        ]
