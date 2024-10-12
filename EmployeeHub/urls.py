36d3e52b21becbaa218976cf698b0b4c0a9bb288
zdenanek <zdenek.kubik@protonmail.com>
2024-10-12 15:18:36 +0200

    feat: base.html and templates' inheritance

--- EmployeeHub/urls.py
+++ EmployeeHub/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
@@ -89,4 +91,4 @@ urlpatterns = [
     path('employees/', UserListView.as_view(), name='employees'),
 
     # path('employee_profile/', employee_profileView.as_view(), name='employee_profile')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
